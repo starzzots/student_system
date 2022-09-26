d1499400c5d7d958cbc98e7e2cd174d3e1621491
Kyle <k_verwey@yahoo.com>
2022-09-26 02:02:28 -0500

    completed school system

---
+++ project/staff/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def login(request):
+    return render(request, 'registration/login.html')
+
+
+def signup(request):
+    return render(request, 'registration/signup.html')
--- project/students/views.py
+++ project/students/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
 from .models import Student
 from .forms import StudentForm
 
