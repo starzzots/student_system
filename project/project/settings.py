8d3361455e5ba2d6e4244e8758404cce505bbbe7
Kyle <k_verwey@yahoo.com>
2022-09-29 16:06:15 -0500

    fixed settings so styling sheet works now

--- project/project/settings.py
+++ project/project/settings.py
@@ -23,11 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY', 'django-insecure-bw1p03b!ymkr4n8ys(&rzt_2y#g52k+0*nqqn(c)fq73*iy+6)')
+SECRET_KEY = 'django-insecure-bw1p03b!ymkr4n8ys(&rzt_2y#g52k+0*nqqn(c)fq73*iy+6)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')) == "1"  # 1 MEANING TRUE
+DEBUG = True  # 1 MEANING TRUE
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
--- students.py
+++ students.py
@@ -1,1 +1,5 @@
-print("helkasdfasdf")
+
+list1 = [1, 2, 3, 4]
+
+print(list1)
+print(list1[::-1])
